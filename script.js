be1ebef9c147842b7d8bbf55581b0cf130557b21
Jasmin Raith <jasmin-raith@outlook.com>
2023-08-09 14:35:52 +0200

    cross and circle animation and click funktion on fields

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ let fields = [
     null
 ];
 
+let currentPlayer = 'circle';
+
 
 function init() {
     render();
@@ -42,11 +44,11 @@ function render() {
             let index = i * 3 + j;
             let symbol = '';
             if (fields[index] === 'circle') {
-                symbol = 'o';
+                symbol = generateCircleSVG();
             } else if(fields[index] === 'cross') {
-                symbol = 'x';
+                symbol = generateCrossSVG();
             }
-            tableHTML += `<td>${symbol}</td>`;
+            tableHTML += `<td onclick="handleClick(this, ${index})">${symbol}</td>`;
         }
         tableHTML += '</tr>';
     }
@@ -54,4 +56,51 @@ function render() {
 
     // set table HTML to contentDiv
     contentDiv.innerHTML = tableHTML;
+}
+
+
+function handleClick(cell, index) {
+    if (fields[index] === null) {
+        fields[index] = currentPlayer;
+        cell.innerHTML = currentPlayer === 'circle' ? generateCircleSVG() : generateCrossSVG();
+        cell.onclick = null;
+        currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
+    }
+}
+
+
+function generateCircleSVG() {
+    let color = '#743deb';
+    let width = 70;
+    let height = 70;
+
+    return `<svg width="${width}" height="${height}">
+              <circle cx="35" cy="35" r="30" stroke="${color}" stroke-width="5" fill="none">
+                <animate attributeName="stroke-dasharray" from="0 188.5" to="188.5 0" dur="0.2s" fill="freeze" />
+              </circle>
+            </svg>`;
+}
+
+
+function generateCrossSVG() {
+    let color = '#ed7811';
+    let width = 70;
+    let height = 70;
+
+    const svgHtml = `
+      <svg width="${width}" height="${height}">
+        <line x1="0" y1="0" x2="${width}" y2="${height}"
+          stroke="${color}" stroke-width="5">
+          <animate attributeName="x2" values="0; ${width}" dur="200ms" />
+          <animate attributeName="y2" values="0; ${height}" dur="200ms" />
+        </line>
+        <line x1="${width}" y1="0" x2="0" y2="${height}"
+          stroke="${color}" stroke-width="5">
+          <animate attributeName="x2" values="${width}; 0" dur="200ms" />
+          <animate attributeName="y2" values="0; ${height}" dur="200ms" />
+        </line>
+      </svg>
+    `;
+
+    return svgHtml;
 }
